d583d6a0a012e16d6e227e5f163a011f9095c0dd
Lunfw <fitia@MSI.localdomain>
2025-11-24 11:02:05 +0100

    repack

--- get_next_line/reine_gnl_I.c
+++ get_next_line/reine_gnl_I.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "get_next_line.h"
 
+// # GNL USES STATIC VARIABLES, OF COURSE YOU CAN ONLY USE ONE TEST
+
 int	main(void)
 {
 	int		fd;
@@ -9,6 +11,7 @@ int	main(void)
 	char	*next_line;
 
 	count = 0;
+	printf("\n\n # ------- BASIC TEST -------\n\n");
 	while (count != 5)
 	{
 		fd = open("example.txt", O_RDONLY);
@@ -17,6 +20,42 @@ int	main(void)
 		printf("[%d]  %s\n", count, next_line);
 		next_line = NULL;
 	}
+	/*
+	printf("\n\n # ------- GET_NEXT_LINE -------\n\n");
+	count = 0;
+	while (count != 5)
+	{
+		fd = open("example_sustainability.txt", O_RDONLY);
+		next_line = get_next_line(fd);
+		count++;
+		printf("[%d]  %s\n", count, next_line);
+		next_line = NULL;
+	}
+	*/
+	/*
+	printf("\n\n # ------- ONE NL TEST -------\n\n");
+	
+	count = 0;
+	while (count != 5)
+	{
+		fd = open("example_oneNL.txt", O_RDONLY);
+		next_line = get_next_line(fd);
+		count++;
+		printf("[%d]  %s\n", count, next_line);
+		next_line = NULL;
+	}
+	*/
+	/*
+	printf("\n\n # ------- MULTIPLE NL TEST -------\n\n');
+	count = 0;
+	while (count != 5)
+	{
+		fd = open("example_multipleNL.txt", O_RDONLY);
+		next_line = get_next_line(fd);
+		count++;
+		printf("[%d]  %s\n", count, next_line);
+		next_line = NULL;
+	}*/
 	free(next_line);
 	close(fd);
 	return (0);
