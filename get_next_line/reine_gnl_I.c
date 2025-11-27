13fca0172bd5e0ea86386ec00d093e05e44242d8
conceive <121719166+Lunfw@users.noreply.github.com>
2025-11-27 12:36:43 +0100

    repack

--- get_next_line/reine_gnl_I.c
+++ get_next_line/reine_gnl_I.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include "get_next_line.h"
 
-// # GNL USES STATIC VARIABLES, OF COURSE YOU CAN ONLY USE ONE TEST
-
 int	main(void)
 {
 	int		fd;
@@ -11,55 +9,49 @@ int	main(void)
 	char	*next_line;
 
 	count = 0;
+	fd = open("get_next_line/example.txt", O_RDONLY);
 	printf("\n # ------- BASIC TEST -------\n\n");
 	while (count != 5)
 	{
-		fd = open("allexamples/example.txt", O_RDONLY);
 		next_line = get_next_line(fd);
 		count++;
 		printf("[%d]  %s\n", count, next_line);
 		next_line = NULL;
 	}
 	printf("\n");
-	/*
 	printf("\n\n # ------- GET_NEXT_LINE -------\n\n");
 	count = 0;
+	fd = open("get_next_line/example_sustainability.txt", O_RDONLY);
 	while (count != 5)
 	{
-		fd = open("allexamples/example_sustainability.txt", O_RDONLY);
 		next_line = get_next_line(fd);
 		count++;
 		printf("[%d]  %s\n", count, next_line);
 		next_line = NULL;
 	}
 	printf("\n");
-	*/
-	/*
 	printf("\n\n # ------- ONE NL TEST -------\n\n");
 	count = 0;
+	fd = open("get_next_line/example_oneNL.txt", O_RDONLY);
 	while (count != 5)
 	{
-		fd = open("allexamples/example_oneNL.txt", O_RDONLY);
 		next_line = get_next_line(fd);
 		count++;
 		printf("[%d]  %s\n", count, next_line);
 		next_line = NULL;
 	}
 	printf("\n");
-	*/
-	/*
-	printf("\n\n # ------- MULTIPLE NL TEST -------\n\n');
+	printf("\n\n # ------- MULTIPLE NL TEST -------\n\n");
 	count = 0;
+	fd = open("get_next_line/example_multipleNL.txt", O_RDONLY);
 	while (count != 5)
 	{
-		fd = open("allexamples/example_multipleNL.txt", O_RDONLY);
 		next_line = get_next_line(fd);
 		count++;
 		printf("[%d]  %s\n", count, next_line);
 		next_line = NULL;
 	}
 	printf("\n");
-	*/
 	free(next_line);
 	close(fd);
 	return (0);
